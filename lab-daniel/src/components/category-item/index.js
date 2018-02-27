75c8b3e333840c0625dfae72b7802edfe075a031
daniellogerstedt <daniel.logerstedt@gmail.com>
2018-02-26 17:09:17 -0800

    added delete functionality

--- lab-daniel/src/components/category-item/index.js
+++ lab-daniel/src/components/category-item/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CategoryForm from '../category-form/';
 import {connect} from 'react-redux';
 import {renderIf} from '../../lib/utils';
-import {categoryUpdate} from '../../actions/category-actions';
+import {categoryUpdate, categoryDelete} from '../../actions/category-actions';
 
 class CategoryItem extends React.Component {
   constructor (props) {
@@ -10,10 +10,15 @@ class CategoryItem extends React.Component {
     this.state = this.props.category;
     this.state.editing = false;
     this.handleDoubleClick = this.handleDoubleClick.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
   
   handleDoubleClick () {
     this.setState({editing: !this.state.editing});
+  }
+
+  handleDelete () {
+    this.props.categoryItemCategoryDelete(this.state);
   }
 
   render() {
@@ -21,6 +26,7 @@ class CategoryItem extends React.Component {
       <div onDoubleClick={this.handleDoubleClick}>
         <h3>Category: {this.props.category.title}</h3>
         <p>Budget: ${this.props.category.budget}</p>
+        <button onClick={this.handleDelete}>Delete</button>
         {renderIf(this.state.editing, <CategoryForm category={this.props.category} buttonText='update' onComplete={this.props.categoryItemCategoryUpdate}/>)}
       </div>
     );
@@ -33,6 +39,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = (dispatch, getState) => ({
   categoryItemCategoryUpdate: category => dispatch(categoryUpdate(category)),
+  categoryItemCategoryDelete: category => dispatch(categoryDelete(category)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CategoryItem);
